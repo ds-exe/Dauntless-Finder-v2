d7937a62a5ac19231f79f6b0947f1a1d4cf449f7
ds-exe <daniel@ds-exe.dev>
2024-10-10 17:40:51 +0100

    Basic project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Dauntless_Finder_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
