6d107ea87bd1f087cdfd2ab3f19245dc4ae374f2
ds-exe <daniel@ds-exe.dev>
2024-10-26 22:33:29 +0100

    Add basic test and benchmark

---
+++ Dauntless-Finder-v2.Test/Benchmarks/BuildFinderBenchmarks.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+using Dauntless_Finder_v2.App.src.Scripts;
+
+namespace Dauntless_Finder_v2.Test.Benchmarks;
+
+public class BuildFinderBenchmarks
+{
+    protected BuildFinder buildFinder { get; set; }
+
+    public BuildFinderBenchmarks()
+    {
+        buildFinder = new BuildFinder();
+    }
+
+    [Benchmark]
+    public void Benchmark1()
+    {
+        List<int> requiredPerks = [];
+        List<int> requestedPerks = new() { 1 };
+        var sut = buildFinder.GetBuilds([5, 8, 17, 24, 30, 36], 50);
+    }
+}
---
+++ Dauntless-Finder-v2.Test/Tests/BuildFinderShould.cs
@@ -0,0 +1,54 @@
+﻿using Dauntless_Finder_v2.App.src.Scripts;
+using NUnit.Framework;
+
+namespace Dauntless_Finder_v2.Test.Tests;
+
+public class BuildFinderShould
+{
+    protected BuildFinder buildFinder { get; set; }
+
+    public BuildFinderShould()
+    {
+
+    }
+
+    [OneTimeSetUp]
+    public void OneTimeSetup()
+    {
+        buildFinder = new BuildFinder();
+    }
+
+    [SetUp]
+    public void SetUp()
+    {
+
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+
+    }
+
+    [Test]
+    public void Test1()
+    {
+        List<int> requiredPerks = [];
+        List<int> requestedPerks = new() { 1 };
+        var sut = buildFinder.GetBuilds([5, 8, 17, 24, 30, 36], 50);
+
+        Assert.That(sut, Is.Not.Null);
+        Assert.That(sut.Count, Is.EqualTo(47));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        List<int> requiredPerks = [];
+        List<int> requestedPerks = new() { 1 };
+        var sut = buildFinder.GetBuilds([5, 8, 17, 24, 30, 36], 20);
+
+        Assert.That(sut, Is.Not.Null);
+        Assert.That(sut.Count, Is.EqualTo(20));
+    }
+}
